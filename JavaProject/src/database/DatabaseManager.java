31d01f8a0e6a801138a754b69e8ec81f4c135e38
okkk2010 <okkk2010@naver.com>
2025-05-24 22:05:56 +0900

    2025.05.24
    client
    - 테스트 frame 생성
    
    server
    - heroku 서버 연동
    - id 검색 api 생성

--- JavaProject/src/database/DatabaseManager.java
+++ JavaProject/src/database/DatabaseManager.java
@@ -1,6 +1,61 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseManager {
 	
+	final private String FOR_NAME_SRC = "com.mysql.cj.jdbc.Driver";
+	final private String DB_URL = "jdbc:mysql://piduf46vuyw3eg9v:cm5iui8ncxt0n1zb@a07yd3a6okcidwap.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/fy96gv7kx2yidmi5";
+	final private String DB_USER = "piduf46vuyw3eg9v";
+	final private String DB_PASSWORD = "cm5iui8ncxt0n1zb";
+	
+	private Connection conn; 
+	
+	public DatabaseManager() {
+		try {
+			Class.forName(FOR_NAME_SRC);
+			conn = DriverManager.getConnection(DB_URL
+					, "piduf46vuyw3eg9v", "cm5iui8ncxt0n1zb");
+			System.out.println("ok");
+		} catch (ClassNotFoundException e) {
+			System.out.println("jdbc driver를 찾지 못했습니다.");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("SQL 연결 실패");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public String getUserIdForName(int id) {
+		try {
+			Statement stmt = conn.createStatement();
+			
+			String selectSQL = "SELECT name FROM testTable where id = (?)";
+			
+			PreparedStatement pStmt = conn.prepareStatement(selectSQL);
+			pStmt.setInt(1, id);
+			
+			ResultSet rs = pStmt.executeQuery();
+			
+			if(rs.next()) {
+				System.out.println(rs.getString("name") + "을 찾았씁니다.");
+				return rs.getString("name");				
+			} else {
+				return "";
+			}
+			
+			
+		} catch (SQLException e) {
+			System.out.println("SQL 연결 실패");
+			e.printStackTrace();
+			return "";
+		}
+		
+	}
 }
--- JavaProject/src/main/MainFrame.java
+++ JavaProject/src/main/MainFrame.java
@@ -6,10 +6,22 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import database.DatabaseManager;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class MainFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField tfInput;
+	private JTextField tfOutput;
+	private static DatabaseManager dbm;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -18,6 +30,7 @@ public class MainFrame extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					dbm = new DatabaseManager();
 					MainFrame frame = new MainFrame();
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -37,6 +50,51 @@ public class MainFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnSeach = new JButton("검색");
+		btnSeach.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int id = Integer.parseInt(tfInput.getText());
+				if(tfInput.getText().length() == 0) {
+					System.out.println("찾으실 아이디를 입력해주세요.");
+					return;
+				}
+				
+				String name = dbm.getUserIdForName(id);
+				
+				if(name.length() == 0) {
+					System.out.println("해당 아이디는 존재하지 않습니다.");
+					return;
+				}
+				
+				
+				tfOutput.setText(name);			
+			}  
+		});
+		btnSeach.setBounds(279, 47, 97, 23);
+		contentPane.add(btnSeach);
+		
+		tfInput = new JTextField();
+		tfInput.setBounds(130, 48, 116, 21);
+		contentPane.add(tfInput);
+		tfInput.setColumns(10);
+		
+		tfOutput = new JTextField();
+		tfOutput.setBounds(130, 95, 116, 21);
+		contentPane.add(tfOutput);
+		tfOutput.setColumns(10);
+		
+		textField = new JTextField();
+		textField.setText("아이디");
+		textField.setBounds(43, 48, 62, 21);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setText("이름");
+		textField_1.setColumns(10);
+		textField_1.setBounds(43, 95, 62, 21);
+		contentPane.add(textField_1);
 	}
-
 }
